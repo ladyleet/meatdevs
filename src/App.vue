0e250d0d6cda43b214ed876de8b06793be11c232
Tracy Lee <ladyleet@rainbows-mac.local>
2019-07-03 15:30:05 -0400

    added newsletter and background images

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">MeatDevs</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <a href="https://mailchi.mp/7fd00f8f4f23/meatdevs">Newsletter</a>
     </div>
     <router-view/>
   </div>
@@ -14,15 +15,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: white;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    font-size: 1.5em;
+    color: white;
     &.router-link-exact-active {
-      color: #42b983;
+      color: red;
     }
   }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="MEAT DEVS"/>
   </div>
 </template>
 
@@ -16,3 +15,12 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 })
 export default class Home extends Vue {}
 </script>
+
+<style>
+  body
+{background-image: url(../assets/sugar-steak-with-bourbon5.png);
+background-size:auto;
+background-repeat:no-repeat;
+} 
+
+</style>
